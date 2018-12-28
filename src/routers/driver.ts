e1b53a253a8734c0503e10e7860bfb98fb83205e
Develop Ix <>
2018-12-28 20:43:48 +0700

    Create get driver by user id

--- src/routers/driver.ts
+++ src/routers/driver.ts
@@ -6,6 +6,7 @@ import { manager } from '../manager/manager';
 export const router = express.Router();
 
 router.get('/', getDriver);
+router.get('/:userid', getDriverByUserId);
 router.post('/', createDriver);
 router.put('/update', updateDriver);
 
@@ -13,6 +14,29 @@ export async function getDriver(req, res, next) {
     let response = undefined;
     try {
       const data = await manager.driver.getDrivers();
+      if (data) {
+        response = resp(data);
+      } else {
+        throw new Error('DATA_NOT_FOUND');
+      }
+    } catch (err) {
+      console.log('err', err);
+      response = resp({ message: err.message }, 400);
+    }
+    next(response);
+}
+
+export async function getDriverByUserId(req, res, next) {
+    let response = undefined;
+    try {
+      let { body } = req;
+      let { userid, orderid } = req.params;
+      let criteria = {
+        'user_id': userid,
+      };
+      let db = di.get('db');
+      let collection = db.collection('drivers');
+      const data = await collection.findOne(criteria);
       if (data) {
         response = resp(data);
       } else {
