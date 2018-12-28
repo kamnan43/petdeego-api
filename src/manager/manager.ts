626ae6d36199a0405ad405ea9fb94185e07bdefb
tanapol <tanapol3s@gmail.com>
2018-12-28 18:30:16 +0700

    fix bug

--- src/manager/manager.ts
+++ src/manager/manager.ts
@@ -1,6 +1,6 @@
 import { OrderManager } from './OrderManager';
 import { QuotationManager } from './QuotationManager';
-import { DriverManager } from './driverManager';
+import { DriverManager } from './DriverManager';
 export const manager = {
   order: new OrderManager(),
   quotation: new QuotationManager(),
