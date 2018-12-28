c5ece862a541fb53617a3ab025b281d9596f6c16
tanapol <tanapol3s@gmail.com>
2018-12-28 16:24:11 +0700

    create order

--- src/routers/order.ts
+++ src/routers/order.ts
@@ -1,5 +1,22 @@
 import * as express from 'express';
+import { di } from '../di';
+import { resp } from '../utils/resp';
 
 export const router = express.Router();
 
-// router.get('', );
+router.post('/', );
+
+export async function createOrder(req, res, next) {
+  let response = undefined;
+  try {
+    let { body } = req;
+    let db = di.get('db');
+    let collection = db.collection('orders');
+    let order = await collection.insertOne(body);
+    response = resp({ result: 'success' }, 200);
+  } catch (err) {
+    console.log('err', err);
+    response = resp({ message: err.message }, 400);
+  }
+  next(response);
+}
