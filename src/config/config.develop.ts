92bedd2c538950d65ea973469ffeed4cf1ea43b1
Sitthi Thiammekha <sitthi@alphaemgroup.com>
2018-12-29 14:39:17 +0700

    edit apiUrl

--- src/config/config.develop.ts
+++ src/config/config.develop.ts
@@ -27,7 +27,7 @@ const config = {
     channelId: '1634637036',
     channelSecret: 'e4804d3d1a51bb8a01058ffd76ecf9fd'
   },
-  apiUrl: 'https://petdee-api.alphaem-dev.tech/api',
+  apiUrl: 'https://petdee-api.alphaem-dev.tech/api/v1',
 };
 
 export default config;
