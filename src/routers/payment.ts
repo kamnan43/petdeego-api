c1f6d79c780956dc09513ac3fd156ca66a00e82c
Sitthi Thiammekha <sitthi@alphaemgroup.com>
2018-12-29 14:39:42 +0700

    move reserve to webhook

--- src/routers/payment.ts
+++ src/routers/payment.ts
@@ -2,44 +2,44 @@ import * as express from 'express';
 import * as rp from 'request-promise';
 import { config } from '../config';
 import { di } from '../di';
-import { getXLineHeader } from '../utils/line';
+// import { getXLineHeader } from '../utils/line';
 
-function reservePayment(req, res) {
-    console.log('reservePayment');
-    let { amount, orderId } = req.query;
-    let url = `${config.linepay.api}/v2/payments/request`;
+// function reservePayment(req, res) {
+//     console.log('reservePayment');
+//     let { amount, orderId } = req.query;
+//     let url = `${config.linepay.api}/v2/payments/request`;
 
-    let payload = {
-        productName: 'PetdeeGo Fee',
-        amount,
-        orderId,
-        currency: 'THB',
-        confirmUrl: `${config.apiUrl}/payment/confirm`,
-        langCd: 'th',
-        confirmUrlType: 'SERVER',
-    };
-    rp({
-        method: 'POST',
-        uri: url,
-        body: payload,
-        headers: getXLineHeader(),
-        json: true,
-    })
-        .then(function (response) {
-            console.log('response', JSON.stringify(response));
-            if (response && response.returnCode === '0000' && response.info) {
-                const data = req.body;
-                const transactionId = response.info.transactionId;
-                data.transactionId = transactionId;
-                updateOrderInfo(orderId, data);
-            }
-            res.send(response);
-        })
-        .catch(function (err) {
-            console.log('payment err', err);
-            res.status(400).send(err);
-        });
-};
+//     let payload = {
+//         productName: 'PetdeeGo Fee',
+//         amount,
+//         orderId,
+//         currency: 'THB',
+//         confirmUrl: `${config.apiUrl}/payment/confirm`,
+//         langCd: 'th',
+//         confirmUrlType: 'SERVER',
+//     };
+//     rp({
+//         method: 'POST',
+//         uri: url,
+//         body: payload,
+//         headers: getXLineHeader(),
+//         json: true,
+//     })
+//         .then(function (response) {
+//             console.log('response', JSON.stringify(response));
+//             if (response && response.returnCode === '0000' && response.info) {
+//                 const data = req.body;
+//                 const transactionId = response.info.transactionId;
+//                 data.transactionId = transactionId;
+//                 updateOrderInfo(orderId, data);
+//             }
+//             res.send(response);
+//         })
+//         .catch(function (err) {
+//             console.log('payment err', err);
+//             res.status(400).send(err);
+//         });
+// };
 
 async function confirmPayment(req, res) {
     let { transactionId, orderId } = req.query;
@@ -112,5 +112,5 @@ async function updateOrderInfo(orderId, body) {
 
 export const router = express.Router();
 
-router.get('/reserve', reservePayment);
-router.post('/confirm', reservePayment);
+// router.get('/reserve', reservePayment);
+router.post('/confirm', confirmPayment);
